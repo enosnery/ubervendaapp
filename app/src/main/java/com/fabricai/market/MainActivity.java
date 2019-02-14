134dcf063f8e0d08fc6133c85eebe26059732e6d
enosnery <enosnery84@gmail.com>
2019-02-14 09:11:11 -0300

    Primeiro commit do app

---
+++ app/src/main/java/com/fabricai/market/MainActivity.java
@@ -0,0 +1,31 @@
+package com.fabricai.market;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings= myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        myWebView.setWebViewClient(new WebViewClient()
+        {
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                view.loadUrl(url);
+                return false;
+            }
+        });
+
+        myWebView.loadUrl("http://35.231.173.150/market");
+    }
+}
