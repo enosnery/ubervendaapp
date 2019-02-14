b76e78dffde70c2f9f42a2d0be539a4737435fd3
enosnery <enosnery84@gmail.com>
2019-02-14 10:00:22 -0300

    Criação da notificação e log do TOKEN de mensagem

--- app/src/main/java/com/fabricai/market/MainActivity.java
+++ app/src/main/java/com/fabricai/market/MainActivity.java
@@ -1,18 +1,45 @@
 package com.fabricai.market;
 
 import android.app.Activity;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
 public class MainActivity extends Activity {
+
+    private static final String TAG = "main activity" ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                                           @Override
+                                           public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                                               if (!task.isSuccessful()) {
+                                                   Log.w(TAG, "getInstanceId failed", task.getException());
+                                                   return;
+                                               }
+                                               String token = task.getResult().getToken();
+
+                                               // Log and toast
+                                               String msg = getString(R.string.msg_token_fmt, token);
+                                               Log.d(TAG, msg);
+                                           }
+
+                                       });
+
 
         WebView myWebView = (WebView) findViewById(R.id.webview);
         WebSettings webSettings= myWebView.getSettings();
